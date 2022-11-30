890fc430ba615223539f2762820cac98b76d060f
dteshuva <98432945+dteshuva@users.noreply.github.com>
2022-11-30 18:31:18 -0500

    Add files via upload

--- MBPixelCalc.s
+++ MBPixelCalc.s
@@ -1,15 +1,12 @@
-	.file	"MBPixelCalc.s"
-	.text
-	.globl	MBPixelCalc
-	.type	MBPixelCalc, @function
+.section .data
+
+.section .text
+.globl MBPixelCalc
+
 MBPixelCalc:
 .LFB0:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	movq	%rdi, -40(%rbp)
 	movq	%rsi, -48(%rbp)
 	movq	$0, -24(%rbp)
@@ -52,10 +49,4 @@ MBPixelCalc:
 .L3:
 	movl	-32(%rbp), %eax
 	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	MBPixelCalc, .-MBPixelCalc
-	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
-	.section	.note.GNU-stack,"",@progbits
